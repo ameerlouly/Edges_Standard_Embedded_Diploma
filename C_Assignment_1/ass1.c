be855a9f74a48edaf2d2def6e6f802298918e1f9
ameerlouly <loulyameer@gmail.com>
2024-07-03 02:16:23 +0300

    first ass finished except for problem 21

---
+++ C_Assignment_1/ass1.c
@@ -0,0 +1,39 @@
+/*
+ * ass1.c
+ *
+ *  Created on: Jul 2, 2024
+ *      Author: test
+ */
+#include<stdio.h>
+#define pi 3.14
+
+//1
+
+void simp1()
+{
+    int x, y;
+    printf("Enter 2 Numbers: ");
+    scanf("%d %d", &x, &y);
+    printf("%d\n\n\n", (x + y) * 3 - 10);
+}
+
+
+//2
+void printName()
+{
+    printf("Ameer Louly\n");
+    printf("Excellent\n\n\n");
+}
+
+//3
+int toF(int c)
+{
+    return c * (9/5) + 32;
+}
+
+//4
+void circlCalc(int r)
+{
+    
+
+}
